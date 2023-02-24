51690f2747f2538248ba2b5de10503d1dadb2468
iamdriz <cameron.drysdale@thefloow.com>
2023-02-24 12:18:55 +0000

    better images

--- imagemin.js
+++ imagemin.js
@@ -1,9 +1,28 @@
 import imagemin from "imagemin";
+import imageminJpegtran from "imagemin-jpegtran";
+import imageminPngquant from "imagemin-pngquant";
 import imageminWebp from "imagemin-webp";
+
+await imagemin(["assets/img/*.{jpg,png}"], {
+  destination: "_assets/img",
+  plugins: [
+    imageminJpegtran({
+      progressive: true,
+      arithmetic: true,
+    }),
+    imageminPngquant({
+      quality: [0.6, 0.8],
+    }),
+  ],
+});
 
 await imagemin(["_assets/img/*.{jpg,png}"], {
   destination: "_assets/img",
-  plugins: [imageminWebp({ quality: 100 })],
+  plugins: [
+    imageminWebp({
+      quality: 80,
+    }),
+  ],
 });
 
 console.log("Images optimized");
